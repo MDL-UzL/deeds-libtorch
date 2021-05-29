0778c62d83951a1f9e361295e50753d9b4aba865
Christian Weihsbach <christian.weihsbach@uni-luebeck.de>
2021-05-29 03:01:06 +0200

    Integrate applyBCV.cpp in py module call

--- py_cpp.py
+++
@@ -1,4 +0,0 @@
-import torch
-torch.ops.load_library("build/libd_sigmoid.dylib")
-sig = torch.ops.deeds_cpp.d_sigmoid(torch.ones(1))
-print(sig)
--- src-python/applyBCV.py
+++ src-python/applyBCV.py
@@ -1,7 +1,9 @@
 import argparse
+import torch
 
 def main():
-    pass
+    torch.ops.load_library("build/libapplyBCV.dylib")
+    torch.ops.deeds_applyBCV.applyBCV_main()
 
 if __name__ == '__main__':
     main()
