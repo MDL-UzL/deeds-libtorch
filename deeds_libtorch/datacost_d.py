87cfe3ea161a75db776a51055bc9cefd2aab54f6
Christian Weihsbach <christian.weihsbach@uni-luebeck.de>
2022-02-22 14:25:11 +0100

    Simplify script

--- deeds_libtorch/datacost_d.py
+++ deeds_libtorch/datacost_d.py
@@ -29,13 +29,11 @@ def warpAffineS(image_seg, affine_mat, w, v, u):
     sw = affine_mat[2,-1]
     sv = affine_mat[1,-1]
     su = affine_mat[0,-1]
-    
+
     displacements = 2.*displacements/(torch.tensor([D,H,W]).view(1,1,1,3))
 
-    # Switch x,y
-    displacements[...,1], displacements[...,0] = displacements.clone()[...,0], displacements.clone()[...,1]
-    displacements[...,2], displacements[...,0] = displacements.clone()[...,0], displacements.clone()[...,2]
-
+    # Switch w,v,u -> u,w,v
+    displacements[...,2], displacements[...,1], displacements[...,0] = displacements.clone()[...,1], displacements.clone()[...,0], displacements.clone()[...,2]
     grid_all = F.affine_grid(affine_mat.unsqueeze(0),(1,1,D,H,W),align_corners=True)
     grid_all += displacements
     warped = F.grid_sample(image_seg.unsqueeze(0).unsqueeze(0).float(), grid_all, mode='nearest', align_corners=True, padding_mode='border')
