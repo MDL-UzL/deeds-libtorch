bdb6af720ee74fff0ea9eb3c28add320541c69fb
Christian Weihsbach <christian.weihsbach@uni-luebeck.de>
2022-02-23 00:44:40 +0100

    Implement warpAffine

--- deeds_libtorch/datacost_d.py
+++ deeds_libtorch/datacost_d.py
@@ -4,10 +4,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def warpAffineS(image_seg, affine_mat, w, v, u):
+def warp(moving, affine_mat, w, v, u, mode='nearest'):
 
+    assert mode in ['nearest', 'bilinear']
+    if mode == 'bilinear':
+        raise NotImplementedError()
+
+    D, H, W = moving.shape
     affine_mat = affine_mat.t()[0:3]
-    D, H, W = image_seg.shape
+    displacements = torch.stack([w,v,u], dim=-1)
+
+    # Rescale displacements
+    displacements = 2.*displacements/(torch.tensor([D,H,W]).view(1,1,1,3))
 
     # Switch x,z
     affine_mat[2,:], affine_mat[0,:] = affine_mat.clone()[0,:], affine_mat.clone()[2,:]
@@ -23,22 +31,58 @@ def warpAffineS(image_seg, affine_mat, w, v, u):
     affine_mat[1,-1] = affine_mat[1,:4].sum()-1.0
     affine_mat[0,-1] = affine_mat[0,:4].sum()-1.0
 
-
-    displacements = torch.stack([w,v,u], dim=-1)
-    # Rescale displacements
-    sw = affine_mat[2,-1]
-    sv = affine_mat[1,-1]
-    su = affine_mat[0,-1]
-
-    displacements = 2.*displacements/(torch.tensor([D,H,W]).view(1,1,1,3))
-
     # Switch w,v,u -> u,w,v
     displacements[...,2], displacements[...,1], displacements[...,0] = displacements.clone()[...,1], displacements.clone()[...,0], displacements.clone()[...,2]
+
     grid_all = F.affine_grid(affine_mat.unsqueeze(0),(1,1,D,H,W),align_corners=True)
     grid_all += displacements
-    warped = F.grid_sample(image_seg.unsqueeze(0).unsqueeze(0).float(), grid_all, mode='nearest', align_corners=True, padding_mode='border')
+    warped = F.grid_sample(moving.unsqueeze(0).unsqueeze(0).float(), grid_all, mode=mode, align_corners=True, padding_mode='border')
 
-    return warped.squeeze(0).squeeze(0).short()
+    warped = warped.squeeze(0).squeeze(0)
+    if mode == 'nearest':
+        return warped.short()
+    elif mode == 'bilinear':
+        return warped
+    else:
+        raise ValueError()
+
+def warpAffineS(label, affine_mat, w, v, u):
+    return warp(label, affine_mat, w, v, u, mode='nearest')
+
+def warpAffine(image, affine_mat, w, v, u):
+    # return warp(image, affine_mat, w, v, u, mode='bilinear')
+    D, H, W = image.shape
+    affine_mat = affine_mat.t()[0:3]
+    displacements = torch.stack([w,v,u], dim=-1)
+
+    # Rescale displacements
+    displacements = 2.*displacements/(torch.tensor([D-1,H-1,W-1]).view(1,1,1,3))
+
+    # # Switch x,z
+    # affine_mat[2,:], affine_mat[0,:] = affine_mat.clone()[0,:], affine_mat.clone()[2,:]
+    # affine_mat[:,2], affine_mat[:,0] = affine_mat.clone()[:,0], affine_mat.clone()[:,2]
+
+    # Rescale displacements
+    affine_mat[0,-1] = (affine_mat[0,-1]/W)*2.0#*affine_mat[0,0]
+    affine_mat[1,-1] = (affine_mat[1,-1]/H)*2.0#*affine_mat[1,1]
+    affine_mat[2,-1] = (affine_mat[2,-1]/D)*2.0#*affine_mat[2,2]
+
+    # Compensate sheering and scaling
+    affine_mat[2,-1] = affine_mat[2,:4].sum()-1.0
+    affine_mat[1,-1] = affine_mat[1,:4].sum()-1.0
+    affine_mat[0,-1] = affine_mat[0,:4].sum()-1.0
+
+    # Switch
+    displacements[...,0], displacements[...,1], displacements[...,2] = displacements.clone()[...,1], displacements.clone()[...,0], displacements.clone()[...,2] #1,2,0
+
+    grid_all = F.affine_grid(affine_mat.unsqueeze(0),(1,1,D,H,W),align_corners=True)
+    grid_all += displacements
+    warped = F.grid_sample(image.unsqueeze(0).unsqueeze(0).float(), grid_all, mode='bilinear', align_corners=True, padding_mode='border')
+
+    warped = warped.squeeze(0).squeeze(0)
+
+    return warped
+
 
 def interp3xyz():
     raise NotImplementedError()
@@ -50,7 +94,4 @@ def dataCostCL():
     raise NotImplementedError()
 
 def warpImageCL():
-    raise NotImplementedError()
-
-def warpAffine():
     raise NotImplementedError()
--- tests/test_datacost_d.py
+++ tests/test_datacost_d.py
@@ -5,7 +5,7 @@ import torch
 import timeit
 
 from __init__ import CPP_APPLY_BCV_MODULE, test_equal_tensors, log_wrapper
-from deeds_libtorch.datacost_d import warpAffineS
+from deeds_libtorch.datacost_d import warpAffineS, warpAffine
 
 
 
@@ -27,14 +27,14 @@ class TestDatacostD(unittest.TestCase):
 
         ## Generate some artificial displacements for x,y,z
         w = torch.ones(D,H,W)
-        v = torch.ones(D,H,W)
-        u = torch.ones(D,H,W)
+        v = torch.zeros(D,H,W)
+        u = torch.zeros(D,H,W)
 
         T = torch.tensor([
-            [0.5, 0.0, 0.0, 0.0],
+            [2.0, 0.0, 0.0, 0.0],
             [0.0, 2.0, 0.0, 0.0],
-            [1.0, 0.0, 2.0, 0.0],
-            [2.0, 0.0, 1.0, 1.0]]
+            [0.0, 0.0, 1.0, 0.0],
+            [0.0, 0.0, 0.0, 1.0]]
         )
 
         #########################################################
@@ -48,6 +48,40 @@ class TestDatacostD(unittest.TestCase):
         # Assert difference
         assert test_equal_tensors(deeds_warped, torch_warped.permute(2,1,0)), "Tensors do not match"
 
+    def test_warpAffine(self):
+        #########################################################
+        # Prepare inputs
+        PAD = 1
+        CONTENT = 5
+        input_img = torch.nn.functional.pad(torch.ones(CONTENT,CONTENT,CONTENT), [PAD]*6)
+        # input_img = torch.arange(CONTENT).float()
+        # input_img = torch.stack([input_img]*CONTENT, 0)
+        # input_img = input_img.view(1,CONTENT,CONTENT)+input_img.view(CONTENT,CONTENT,1)+input_img.view(CONTENT,1,CONTENT)
+        D, H, W = input_img.shape
+
+        ## Generate some artificial displacements for x,y,z
+        w = torch.ones(D,H,W)
+        v = 1.1*torch.ones(D,H,W)
+        u = 1.2*torch.ones(D,H,W)
+
+        T = torch.tensor([
+            [0.5, 0.0, 0.0, 0.0],
+            [0.2, 0.5, 0.0, 0.0],
+            [0.0, 0.0, 1.0, 0.0],
+            [0.0, 0.0, 0.0, 1.0]]
+        )
+
+        #########################################################
+        # Get cpp output
+        cpp_warped = log_wrapper(CPP_APPLY_BCV_MODULE.datacost_d_warpAffine, input_img, T, w, v, u)
+
+        #########################################################
+        # Get torch output
+        torch_warped = log_wrapper(warpAffine, input_img, T.t(), w, v, u)
+        #########################################################
+        # Assert difference
+        assert test_equal_tensors(cpp_warped, torch_warped), "Tensors do not match"
+
     def test_interp3xyz(self):
         assert False
 
@@ -60,12 +94,10 @@ class TestDatacostD(unittest.TestCase):
     def test_warpImageCL(self):
         assert False
 
-    def test_warpAffine(self):
-        assert False
-
 
 
 if __name__ == '__main__':
     # unittest.main()
     tests = TestDatacostD()
-    tests.test_warpAffineS()
+    # tests.test_warpAffineS()
+    tests.test_warpAffine()
