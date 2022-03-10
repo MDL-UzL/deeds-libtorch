82e936e9bd596a28457e0f448ea4ccbc09356c05
Christian Weihsbach <christian.weihsbach@uni-luebeck.de>
2022-03-10 12:11:04 +0100

    Improve MIND testcase

--- tests/test_mind_ssc.py
+++ tests/test_mind_ssc.py
@@ -10,7 +10,7 @@ import nibabel as nib
 from deeds_libtorch.mind_ssc import mind_ssc, filter1D
 
 
-from __init__ import SRC_DIR, BUILD_DIR, BUILD_JIT_DIR, CPP_DEEDS_MODULE, test_equal_tensors, log_wrapper
+from __init__ import SRC_DIR, BUILD_DIR, BUILD_JIT_DIR, CPP_DEEDS_MODULE, test_equal_tensors, log_wrapper, mind_compress_features, mind_extract_features, mind_twelve_to_long, mind_long_to_twelve
 
 def extract_features(binaries):
     D,H,W,DIGITS = binaries.shape
@@ -29,31 +29,25 @@ class TestMindSsc(unittest.TestCase):
         #########################################################
         # Prepare inputs
         QUANTISATION_STEP = 1
-        D, H, W =  3, 3, 3
+        D, H, W =  5, 5, 5
 
         ## Generate some artificial displacements for x,y,z
-        image = torch.zeros((D,H,W))
-        image[1,1,1] = 1
+        image = torch.ones((D,H,W))
+        image[1,1,1] = 2
+        image[2,2,2] = -1
         #########################################################
         # Get cpp output
         cpp_mind_ssc_long_long, cpp_mind_ssc_twelve, mind_bare = CPP_DEEDS_MODULE.mind_ssc_descriptor(
             image,
             torch.tensor([QUANTISATION_STEP], dtype=torch.int)
         )
-        cpp_mind_ssc = cpp_mind_ssc_twelve.permute(-1,0,1,2)
+        cpp_mind = mind_long_to_twelve(cpp_mind_ssc_long_long)
 
         #########################################################
         # Get torch output
-        torch_mind_ssc = mind_ssc(image, QUANTISATION_STEP, sigma=10.0)
-
+        torch_mind_ssc = mind_ssc(image, QUANTISATION_STEP, use_smoothing=False, sigma=0.8, compensation_factor=5.)
         torch_mind_ssc = torch_mind_ssc.reshape(12,D,H,W)
-        # def binary(x, bits):
-        #     mask = 2**torch.arange(bits).to(x.device, x.dtype)
-        #     return x.unsqueeze(-1).bitwise_and(mask).ne(0).byte()
-        # BITS_PER_FEATURE = 5
-        # NUM_FEAURES = 12
-        # binaries = binary(cpp_mind_ssc, 64)[...,:BITS_PER_FEATURE*NUM_FEAURES]
-        # features = extract_features(binaries)
+
         #########################################################
         # Assert difference
         assert test_equal_tensors(cpp_mind_ssc, torch_mind_ssc)
